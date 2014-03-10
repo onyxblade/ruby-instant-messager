04283f1c9476825a1e79fbe9d53ba19c34bd728c
Cichol <cichol@live.cn>
2014-03-10 22:39:56 +0800

    detect off-line using rescue

--- server.rb
+++ server.rb
@@ -14,23 +14,28 @@ module ClientManager
   def self.send_to_all(data)
     puts data
     @clients.each do |c|
-      c.body.send(data, 0)
+        c.body.send(data, 0) if c.recv_thread.alive?
     end
   end
 end
 
 class Client
-  attr_accessor :body
+  attr_accessor :body, :recv_thread, :name
 
   def initialize(c)
     @body = c
     @name = @body.recv(150, 0)
     puts "Client accepted. Whose name is #{@name}."
     @recv_thread = Thread.new do
-      loop do
-        data = @body.recv(150, 0)
-        ClientManager.send_to_all(@name + "   #{Time.now}\n")
-        ClientManager.send_to_all(data)
+      begin
+        loop do
+          data = @body.recv(150, 0)
+          ClientManager.send_to_all(@name + "   #{Time.now}\n")
+          ClientManager.send_to_all(data)
+        end
+      rescue
+        puts "#{self.name} off-lined.\n"
+        @clients.delete(self)
       end
     end
   end
